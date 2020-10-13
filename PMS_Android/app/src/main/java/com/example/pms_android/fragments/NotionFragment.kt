415e30436ed5a43767b60ab6baebce9429a8b540
jaewonkim <turitus@naver.com>
2020-10-13 17:11:10 +0900

    .

--- PMS_Android/app/src/main/java/com/example/pms_android/fragments/NotionFragment.kt
+++ PMS_Android/app/src/main/java/com/example/pms_android/fragments/NotionFragment.kt
@@ -14,6 +14,7 @@ class NotionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_notion, container, false)
     }
 }
