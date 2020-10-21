4f2806e370fe4ba9db2de42c685a47a9a49cebc9
jaewonkim <turitus@naver.com>
2020-10-21 23:11:18 +0900

    [ADD]icon

--- PMS_Android/app/src/main/java/com/example/pms_android/customview/CheckPasswordEditText.kt
+++ PMS_Android/app/src/main/java/com/example/pms_android/customview/CheckPasswordEditText.kt
@@ -1,6 +1,7 @@
 package com.example.pms_android.customview
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
@@ -13,26 +14,22 @@ import com.example.pms_android.R
 import kotlinx.android.synthetic.main.activity_signup.*
 import kotlinx.android.synthetic.main.activity_signup.view.*
 
-class CheckPasswordEditText(context: Context,attrs:AttributeSet) : AppCompatEditText(context,attrs),TextWatcher {
+class CheckPasswordEditText(context: Context, attrs: AttributeSet) :
+    AppCompatEditText(context, attrs), TextWatcher {
+    private var checkDrawble:Drawable = ContextCompat.getDrawable(getContext(),R.drawable.ic_baseline_done_24)!!
 
-    companion object{
+    companion object {
         var checkPasswordCheck = false
     }
-    init{
-        context.theme.obtainStyledAttributes(
-            attrs,
-            R.styleable.CheckPasswordEditText,
-            0,0
-        ).apply {
-        try{
 
-        }
-        finally {
+    init {
 
-        }
-        }
+        checkDrawble.setBounds(0,0,checkDrawble.intrinsicWidth,checkDrawble.intrinsicHeight)
+        checkDrawble.setVisible(true,false)
+        setCompoundDrawables(null,null,checkDrawble,null)
+        addTextChangedListener(this)
+        setIcVisibility(true)
     }
-
 
     private fun checkErrorCheck() {
         if (signup_check_password.text.toString() != signup_check_password.text.toString()) {
@@ -40,14 +37,16 @@ class CheckPasswordEditText(context: Context,attrs:AttributeSet) : AppCompatEdit
             signup_check_password_layout.error = "비밀번호가 다릅니다"
             checkPasswordCheck = false
         } else {
-            signup_check_password_layout.isErrorEnabled = false
+            signup_check_password_layout!!.isErrorEnabled = false
             signup_check_password_layout.error = null
             checkPasswordCheck = true
         }
     }
+
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
         checkErrorCheck()
     }
+
     override fun afterTextChanged(s: Editable?) {
         checkErrorCheck()
     }
@@ -55,5 +54,8 @@ class CheckPasswordEditText(context: Context,attrs:AttributeSet) : AppCompatEdit
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
         signup_get_password_layout.isPasswordVisibilityToggleEnabled = false
     }
-
+private fun setIcVisibility(bool:Boolean){
+    checkDrawble.setVisible(bool,false)
+    setCompoundDrawables(null,null,if(bool) checkDrawble else null,null)
+}
 }
