322dcc74fed92ecc140e45dd0747e8aa2d392561
이은별 <lbj4298@naver.com>
2020-11-08 13:12:50 +0900

    [ADD] 동아리 소개 페이지

---
+++ PMS_Android/app/src/main/java/com/example/pms_android/introduceclubActivity.kt
@@ -0,0 +1,17 @@
+package com.example.pms_android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_introduce.*
+
+class introduceclubActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_introduceclub)
+    }
+}
