64cbe855a483592db6c037e26fc55579d7b313d2
이은별 <lbj4298@naver.com>
2020-11-08 22:20:36 +0900

    [MODIFY]파일명 수정
    파일의 맨 앞글자는 대문자

--- PMS_Android/app/src/main/java/com/example/pms_android/fragments/IntroduceFragment.kt
+++ PMS_Android/app/src/main/java/com/example/pms_android/fragments/IntroduceFragment.kt
@@ -1,14 +1,11 @@
 package com.example.pms_android.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pms_android.R
-import com.example.pms_android.introduceclubActivity
-import kotlinx.android.synthetic.main.fragment_introduce.*
 
 
 class IntroduceFragment : Fragment() {
