5924f3f06d2cd2827d11b5b5420a1d96c1f325c3
이은별 <lbj4298@naver.com>
2020-11-12 08:34:20 +0900

    [MODIFY] 잘못된 파일명 수정

--- PMS_Android/app/src/main/java/com/example/pms_android/PasswordActivity.kt
+++ PMS_Android/app/src/main/java/com/example/pms_android/PasswordActivity.kt
@@ -3,7 +3,7 @@ package com.example.pms_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class passwordActivity : AppCompatActivity() {
+class PasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password)
