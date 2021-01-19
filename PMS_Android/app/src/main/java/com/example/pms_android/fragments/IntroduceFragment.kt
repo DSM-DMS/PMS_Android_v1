2d9cb0d7b998312bfa9a6720cbec8a84abf0cca9
dlswer23 <lbj4298@naver.com>
2021-01-19 18:04:39 +0900

    [ADD]

--- PMS_Android/app/src/main/java/com/example/pms_android/fragments/IntroduceFragment.kt
+++ PMS_Android/app/src/main/java/com/example/pms_android/fragments/IntroduceFragment.kt
@@ -1,11 +1,14 @@
 package com.example.pms_android.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.pms_android.Activity.IntroduceClubActivity
 import com.example.pms_android.R
+import kotlinx.android.synthetic.main.fragment_introduce.*
 
 
 class IntroduceFragment : Fragment() {
@@ -16,5 +19,11 @@ class IntroduceFragment : Fragment() {
         // Inflate the layout for this fragment
 
         return inflater.inflate(R.layout.fragment_introduce, container, false)
+
+        //fragment
+        /*cardview1.setOnClickListener(){
+            startActivity(Intent(this, IntroduceClubActivity::class.java))
+            finish()
+        }*/
     }
 }
