e299b4b9cc78a4ed01104e030de260a10c0c0f5d
dlswer23 <lbj4298@naver.com>
2021-01-19 16:17:29 +0900

    [ADD] introduce_club 세부페이지

---
+++ PMS_Android/app/src/main/java/com/example/pms_android/introduce_detail/Introduce_Club_Detail_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.pms_android.introduce_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pms_android.R
+
+class Introduce_Club_Detail_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_introduce__club__detail_)
+    }
+}
