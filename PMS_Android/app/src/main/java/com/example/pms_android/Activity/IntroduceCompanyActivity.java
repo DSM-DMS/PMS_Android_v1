713e2021763c64652d4d67a3026233b9ecb70bd8
dlswer23 <lbj4298@naver.com>
2021-01-19 14:44:40 +0900

    [ADD] 회사 소개 +

---
+++ PMS_Android/app/src/main/java/com/example/pms_android/Activity/IntroduceCompanyActivity.java
@@ -0,0 +1,14 @@
+package com.example.pms_android.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroduceCompanyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduce_company);
+    }
+}
