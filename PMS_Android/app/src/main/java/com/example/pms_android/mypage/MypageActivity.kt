4221852bb5d45965a7a8ee1325ed674011218d1a
dlswer23 <lbj4298@naver.com>
2021-01-27 01:02:39 +0900

    [ADD] Intent

--- PMS_Android/app/src/main/java/com/example/pms_android/mypage/MypageActivity.kt
+++ PMS_Android/app/src/main/java/com/example/pms_android/mypage/MypageActivity.kt
@@ -1,11 +1,24 @@
 package com.example.pms_android.mypage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.pms_android.R
+import kotlinx.android.synthetic.main.activity_mypage.*
 
 class MypageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
+
+        //비밀번호 변경 액티비티로 넘어가기
+        background5.setOnClickListener{
+            val intent = Intent (this, PasswordActivity::class.java)
+            startActivity(intent);
+
+
+
+        }
     }
 }
--- PMS_Android/app/src/main/java/com/example/pms_android/mypage/PasswordActivity.kt
+++ PMS_Android/app/src/main/java/com/example/pms_android/mypage/PasswordActivity.kt
@@ -1,11 +1,20 @@
-package com.example.pms_android
+package com.example.pms_android.mypage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pms_android.R
+import kotlinx.android.synthetic.main.activity_password.*
 
 class PasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password)
+
+        //비밀번호 재설정
+        Check_btn.setOnClickListener{
+
+        }
+
+
     }
 }
